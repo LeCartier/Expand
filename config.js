1dba3c4fb33793eed16a0e1815c2e99fd099ad85
LeCartier <carter.daniel13@gmail.com>
2025-11-17 16:53:52 -0500

    updated key

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // Minimal central config. Replace with your real project values.
 window.APP_CONFIG = {
   SUPABASE_URL: "https://qexrcpbaczsjwrzsykui.supabase.co",
-  SUPABASE_ANON_KEY: "sb_publishable_7LY38_Chm0oH11QgDjfOlQ_A2i_8U2T",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFleHJjcGJhY3pzandyenN5a3VpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0MDEzNDYsImV4cCI6MjA3ODk3NzM0Nn0.zB0YJ2Pnu_2zYH70JVqs4ltWDMHfFX0jeiwizIMeXn0",
   VIEWER_BASE_URL: "https://lecartier.github.io/Expand/view.html"
 };
